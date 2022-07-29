8cbe362257ba892304ebd88baccdf3d8836a33a4
mjlee76 <dlalswns0422@gmail.com>
2022-07-29 18:24:01 +0900

    scanner, bufferedReader 사용한 EOF

--- 백준/백준/반복문/에이쁠비.java
+++ 백준/백준/반복문/에이쁠비.java
@@ -1,17 +1,43 @@
+// package 백준.반복문;
+
+// import java.util.*;
+
+// public class 에이쁠비 {
+//     public static void main(String args[]){
+
+//         Scanner sc = new Scanner(System.in);
+
+//         while(sc.hasNextInt()){
+
+//             int A = sc.nextInt();
+//             int B = sc.nextInt();
+
+//         }
+//         System.out.println(A+B);
+//     }
+// }
+
 package 백준.반복문;
 
+import java.io.*;
 import java.util.*;
 
 public class 에이쁠비 {
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
 
-        while(1){
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+        while((str=br.readLine())!=null){
+
+            st = new StringTokenizer(str," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+
         }
-
-        System.out.println(A+B);
+        System.out.println(sb);
     }
 }
