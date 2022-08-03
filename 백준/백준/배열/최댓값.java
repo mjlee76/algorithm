7ece94e9740ebe263374621a7faba6300edda745
mjlee76 <dlalswns0422@gmail.com>
2022-08-03 16:34:31 +0900

    22.08.03

---
+++ 백준/백준/배열/최댓값.java
@@ -0,0 +1,24 @@
+package 백준.배열;
+
+import java.util.*;
+
+public class 최댓값 {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        int num[] = new int[8];
+
+        for(int i=0; i<8; i++)
+            num[i] = sc.nextInt();
+
+        int max = num[0];
+        int j=0;
+        while(j<9){
+            if(num[j] < num[j+1]){}
+        }
+
+    }
+    
+}
